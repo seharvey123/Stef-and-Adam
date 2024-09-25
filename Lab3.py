fac453b9b27f38530dd0937cd1fe3f49d32cd6cd
adamb <abhatti@potomacschoool.org>
2024-09-25 18:38:43 -0400

    second

--- Lab3.py
+++ Lab3.py
@@ -1,26 +1,20 @@
 import random
 
-
-print("test")
-
-
-#
-# def list_change(arr2):
-#     for i in range(len(arr2)):
-#         arr2[i] += 5
-
-
 ar_verbs = ['hablar', 'cantar', 'bailar','trabajar','estudiar','caminar','escuchar','llevar','comprar','mirar']
 ir_verbs = ['correr', 'vender', 'aprender','comprender','creer','romer','temer','comer','beber','comer']
 er_verbs = ['vivir', 'escribir', 'abrir','recibir','comprartir','decidir','subir','insistir','permitir','existir']
 
-verb_type = input('Please enter what type of verb you want to practice')
+def get_verb(verb_type):
+    if verb_type == 'ar':
+        print (random.choice(ar_verbs))
+    elif verb_type == 'er':
+        print (random.choice(er_verbs))
+    elif verb_type == 'ir':
+        print (random.choice(ir_verbs))
+    else:
+        print ("Invalid Verb Type")
 
-if ver_type='ar'
-    for element in ar_verbs:
-         print ()
-
-print(ar_verbs)
-print(ir_verbs)
-print(er_verbs)
+while True: #to make it go forever, it wouldn't be helpful to just use this tool once
+    verb_type = input('Please enter what type of verb you want to practice, ar, er, or ir ')
+    get_verb(verb_type)
 
